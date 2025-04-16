6ba721dae28b330042e8b25b5c3cd00ed89de898
M0nitry <47572770+GherghelasAlexandru@users.noreply.github.com>
2025-04-16 12:09:11 +0200

    deploy update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: '',
-  assetPrefix: '',
+  output: 'export',     
+  basePath: '',         
+  assetPrefix: '',     
 };
 
 module.exports = nextConfig;
